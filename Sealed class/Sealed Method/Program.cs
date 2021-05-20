9e523d034b5d78ec6172c8ac0be41cfd5199cf78
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-20 21:14:10 +0530

    Add files via upload

---
+++ Sealed class/Sealed Method/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Sealed_Method
+{
+    class Printer
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("printer printing....\n");
+        }
+    }
+    class Class1 : Printer
+    {
+        sealed override public void print()                                 // Sealed Display Functionto override Print() function
+        {
+            Console.WriteLine("Class1 printing....\n");
+        }
+    }
+    //class Class2 : Class1
+    //{
+    //    override public void print()            // Class2 class cannot override print function as it is sealed in Class1 class.
+    //    {
+    //        Console.WriteLine("Class2 printing....");
+    //    }
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer();
+            p.print();
+
+            Printer c1 = new Class1();
+            c1.print();
+
+            //Printer c2 = new Class2();
+            //c2.print();
+        }
+    }
+}

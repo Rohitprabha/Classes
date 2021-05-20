1d8c16ffa8772ad48c5fc144919de813e3aa3937
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-20 21:11:23 +0530

    Add files via upload

---
+++ Sealed class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sealed_Classes
+{
+    sealed class SealedClass                                // Sealed class 
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+    //class DerivedClass : SealedClass                        //'DerivedClass': cannot derive from sealed type 'SealedClass'
+    //{
+
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SealedClass sealedCls = new SealedClass();
+            int total = sealedCls.Add(4, 5);
+            Console.WriteLine("Total = " + total.ToString());
+        }
+    }
+}
